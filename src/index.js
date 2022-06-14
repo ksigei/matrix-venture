17b5cc45efd9006a615fb375a9a8177827fa970c
sigeikiprono4@gmail.com <sigeikiprono4@gmail.com>
2022-06-14 22:55:44 +0300

    Add navbar

--- src/index.js
+++ src/index.js
@@ -3,4 +3,5 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'typeface-roboto';
 import '@fontsource/raleway';
 
-import './styles.css';
+import './css/styles.css';
+import './css/header.css';
--- webpack.config.js
+++ webpack.config.js
@@ -24,7 +24,7 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       title: 'webpack Boilerplate',
-      template: path.resolve(__dirname, './src/template.html'), // template file
+      template: path.resolve(__dirname, './src/index.html'), // template file
       filename: 'index.html', // output file
     }),
     new CleanWebpackPlugin(),
