9be852fdf59c27e6d547ac96edd4aab5a3be87bc
sigeikiprono4@gmail.com <sigeikiprono4@gmail.com>
2022-06-19 11:18:21 +0300

    Create divs, sections, cards with basic styling

--- src/index.js
+++ src/index.js
@@ -5,3 +5,14 @@ import '@fontsource/raleway';
 
 import './css/styles.css';
 import './css/header.css';
+
+// images
+import logo from './assets/logo.svg';
+import puzzle from './assets/puzzle2.svg';
+import balance from './assets/balance2.svg';
+import plant from './assets/plant2.svg';
+
+document.getElementById('logo').src = logo;
+document.getElementById('puzzle').src = puzzle;
+document.getElementById('balance').src = balance;
+document.getElementById('plant').src = plant;
